3ddd33a5a260c3388c20ff2f89eed5ad94e922b5
André Machado <andrels.machado82@gmail.com>
2019-10-22 19:50:36 -0300

    Configurando sequelize parte 2

--- src/config/database.js
+++ src/config/database.js
@@ -2,8 +2,8 @@ module.exports = {
   dialect: 'postgres',
   host: 'localhost',
   username: 'postgres',
-  password: '12345',
-  database: 'gympoint',
+  password: 'gympoint',
+  database: 'postgres',
   define: {
     timestamps: true,
     underscored: true,
