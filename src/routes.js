18e031fbd9f4ec2b03104772eb651137617b1bd6
André Machado <andrels.machado82@gmail.com>
2019-10-22 15:53:26 -0300

    Configurando sucrase e nodemon

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import {Router} from 'express';
+
+const routes = new Router();
+
+routes.get('/user', (req, res) => {
+  return res.json({message: 'André Luis Machado'})
+})
+
+export default routes;
