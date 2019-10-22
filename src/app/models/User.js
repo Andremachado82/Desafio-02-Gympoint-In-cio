0fb8ccb0ef24729b9d4fd1faca55c4d06ea84d5d
André Machado <andrels.machado82@gmail.com>
2019-10-22 20:41:35 -0300

    Criação de usuário

---
+++ src/app/models/User.js
@@ -0,0 +1,22 @@
+import { Model, Sequelize } from 'sequelize';
+import bcrypt from 'bcryptjs';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
+        password_hash: Sequelize.STRING,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    this.addHook('beforeSave');
+  }
+}
+
+export default User;
