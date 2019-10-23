d71d3c9bbae653c5f046c89a4b6deb15ddbad8af
André Machado <andrels.machado82@gmail.com>
2019-10-22 21:19:33 -0300

    Separando token jwt

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken';
 
+import authConfig from '../../config/auth';
 import User from '../models/User';
 
 class SessionController {
@@ -23,8 +24,8 @@ class SessionController {
         name,
         email,
       },
-      token: jwt.sign({ id }, 'cb3c833675ea8ab78f9b1dce86c72db8', {
-        expiresIn: '7d',
+      token: jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
       }),
     });
   }
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'cb3c833675ea8ab78f9b1dce86c72db8',
+  expiresIn: '7d',
+};
